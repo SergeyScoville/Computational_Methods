8c5db71ef052a0313b8d03b549820b5a60ac7dc4
sscovill <sscovill@cern.ch>
2024-09-06 15:59:29 -0400

    Editing layout and adding plotting

--- src/plotting.cpp
+++ src/plotting.cpp
@@ -4,14 +4,18 @@
 
 int main() {
     // Name of the existing data file
-    std::string fileName = "asst1-1.txt";
+    std::string fileName;
+    std::cout << "Please enter the name of the data file you want to plot: ";
+    std::cin >> fileName;
 
     // Invoke gnuplot to plot the data
     std::string plotCommand = 
         "gnuplot -persist <<-EOFMarker\n"
-        "set title \"y vs. x with Error Bars\"\n"
+        "set title \"y as a function of x\" font \",18\"\n"
         "set xlabel \"x\"\n"
         "set ylabel \"y\"\n"
+        "set grid xtics ytics mxtics mytics lw 1 lt 0 lc rgb \"gray\"\n"
+        "set terminal qt font 'Arial,12'\n"
         "plot \"" + fileName + "\" using 1:2:3 with yerrorbars lt 1 lw 2 pt 7 ps 1.0 lc rgb 'blue' title \"Data\"\n"
 		"EOFMarker\n";
 
